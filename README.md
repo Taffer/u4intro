c58133d0398435f384a8230f50e144752015583b
Chris Herborth <chrish@pobox.com>
2022-09-16 17:45:24 -0400

    Second version, now with Daemon/Dragon

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # u4intro
-Simulating the Ultima IV intro
+
+Simulating the [Ultima IV intro](https://www.youtube.com/watch?v=16pspR74M6w)
+with [PyGame](https://www.pygame.org/).
+
+Ever since I played *Ultima IV: Quest of the Avatar* on my C=64 I've been
+fascinated with the cool graphics in the intro. Here's an attempt to
+reproduce them using PyGame.
+
+![Partially simulated Ultima IV intro animation](./u4intro-2.gif)
+
+You can get Ultima IV for free from
+[GOG.com](https://www.gog.com/game/ultima_4); this expects to find it installed
+in `D:\Games\GOG\Ultima 4` (see `U4INTRO` in the code). Specifically it's
+looking for `TITLE.EXE`, the DOS version of the intro, which is going to
+provide some data.
+
+Special thanks to
+[Ultima Codex](https://wiki.ultimacodex.com/wiki/Ultima_IV_internal_formats#TITLE.EXE)
+for their reverse engineering docs and the `Ultima4_LZW_Animate.png` file that
+I use for the Daemon and Dragon animation frames (cheating a bit because I
+don't want to decode the data from the original files).
+
+The code also uses [pygame_recorder](https://github.com/tdrmk/pygame_recorder)
+for saving the screen frames as an AVI file, which I've been converting into
+GIF for posting online.
+
+## TODO
+
+* Make the Daemon and Dragon scroll down from the top.
+* Add ULTIMA logo fade-in.
---
+++ u4intro.py
@@ -0,0 +1,153 @@
+#! /usr/bin/env python3
+''' Simulate the Ultima IV: Quest of the Avatar intro.
+
+By Chris Herborth (https://github.com/Taffer)
+MIT license, see LICENSE.md for details.
+'''
+
+import pygame
+import pygame.gfxdraw
+import pygame_recorder  # https://github.com/tdrmk/pygame_recorder
+import time
+import sys
+
+SCREEN_TITLE = 'Not Ultima IV'
+
+SCREEN_WIDTH = 320  # 320x240, state of the art in 1985-ish...
+SCREEN_HEIGHT = 240
+
+BLACK = pygame.Color('black')
+WHITE = pygame.Color('white')
+
+TARGET_FPS = 30
+RECORDING = True
+
+# https://wiki.ultimacodex.com/wiki/Ultima_IV_internal_formats#TITLE.EXE
+U4INTRO = r'D:\Games\GOG\Ultima 4\TITLE.EXE'  # Why yes, this is terrible.
+U4_BEASTIE_OFFSET = 0x7380
+U4_DAEMON_WIDTH = 56
+U4_DRAGON_WIDTH = 48
+U4_BEASTIE_HEIGHT = 32
+U4_SIG_OFFSET = 0x746e
+
+
+class Demo:
+    def __init__(self, screen, record=False):
+        self.screen = screen
+        self.clock = pygame.time.Clock()
+        self.sig_length = 1
+        self.beastie_frame = 0
+        self.beastie_ticks = 0
+
+        if record:
+            self.recorder = pygame_recorder.ScreenRecorder(SCREEN_WIDTH, SCREEN_HEIGHT, TARGET_FPS)
+        else:
+            self.recorder = None
+
+        self.intro_bytes = open(U4INTRO, 'rb').read()
+
+        # Animation frames for the top-left and top-right beasties. There are
+        # 18 available frames. 184 frames interleaved in the animation data.
+        #
+        # The frames are 56x32 starting at 0,0 for the daemon and 176,0 for
+        # the dragon. The dragon is 48x32.
+        self.beastie = pygame.image.load('Ultima4_LZW_Animate.png').convert_alpha()
+        self.daemon_frames = []
+        self.dragon_frames = []
+        daemon_x = 0
+        dragon_x = 176
+        y = 0
+        beastie_rect = self.beastie.get_rect()
+        for i in range(18):
+            rect = pygame.Rect(daemon_x, y, U4_DAEMON_WIDTH, U4_BEASTIE_HEIGHT)
+            self.daemon_frames.append(rect)
+
+            rect = pygame.Rect(dragon_x, y, U4_DRAGON_WIDTH, U4_BEASTIE_HEIGHT)
+            self.dragon_frames.append(rect)
+
+            y += U4_BEASTIE_HEIGHT
+            if y + U4_BEASTIE_HEIGHT > beastie_rect.height:
+                daemon_x += U4_DAEMON_WIDTH
+                dragon_x += U4_DRAGON_WIDTH
+                y = 0
+
+        self.daemon_ani = []
+        self.dragon_ani = []
+        for i in range(0, 184, 2):
+            self.daemon_ani.append(int(self.intro_bytes[U4_BEASTIE_OFFSET + i]))
+            self.dragon_ani.append(int(self.intro_bytes[U4_BEASTIE_OFFSET + i + 1]))
+
+        # Signature is 266 x,y co-ordinates.
+        self.signature = []
+        for i in range(0, 532, 2):
+            # Y co-ords are upside down due to DOS screen buffers growing up
+            # from the bottom.
+            xy = (int(self.intro_bytes[U4_SIG_OFFSET + i]), int(SCREEN_HEIGHT - self.intro_bytes[U4_SIG_OFFSET + i + 1]))
+            self.signature.append(xy)
+
+    def draw(self):
+        self.screen.fill(BLACK)
+
+        for i in range(self.sig_length):
+            (x, y) = self.signature[i]
+            pygame.gfxdraw.pixel(self.screen, x, y, WHITE)
+
+        # Draw daemon, top-left.
+        rect = pygame.Rect(0, 0, U4_DAEMON_WIDTH, U4_BEASTIE_HEIGHT)
+        self.screen.blit(self.beastie, rect, self.daemon_frames[self.daemon_ani[self.beastie_frame]])
+
+        # Draw dragon, top-right.
+        rect = pygame.Rect(SCREEN_WIDTH - U4_DRAGON_WIDTH, 0, U4_DRAGON_WIDTH, U4_BEASTIE_HEIGHT)
+        self.screen.blit(self.beastie, rect, self.dragon_frames[self.dragon_ani[self.beastie_frame]])
+
+        self.beastie_ticks += 1
+        if self.beastie_ticks % 10 == 0:
+            self.beastie_frame += 1
+            if self.beastie_frame >= len(self.daemon_ani):
+                self.beastie_frame = 0
+
+    def update(self):
+        self.sig_length += 1
+        if self.sig_length > len(self.signature):
+            self.sig_length = 1
+
+            if self.recorder:
+                pygame.event.post(pygame.event.Event(pygame.QUIT, {}))
+
+        if self.recorder:
+            self.recorder.capture_frame(self.screen)
+
+        self.clock.tick(TARGET_FPS)
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption(SCREEN_TITLE)
+
+    demo = Demo(screen, RECORDING)
+
+    playing = True
+
+    while playing:
+        demo.draw()
+        pygame.display.flip()
+
+        demo.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                playing = False
+
+    if demo.recorder:
+        demo.recorder.end_recording()
+
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
